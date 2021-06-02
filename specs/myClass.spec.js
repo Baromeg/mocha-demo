fa7bd4d8a11ca118da7d8b3592b8ddacee7078be
Baltasar Romero <70645738+Baromeg@users.noreply.github.com>
2021-06-02 19:49:27 +0100

    Using Chai

--- specs/myClass.spec.js
+++ specs/myClass.spec.js
@@ -1,1 +1,16 @@
+// Import MyClass and Chai
 let MyClass = require('../src/myClass')
+let myObj = new MyClass()
+// Assertion library Chai
+let chai = require('chai')
+let expect = chai.expect
+// Spy library Sinon
+let sinon = require('sinon')
+
+// Test suit - Using describe block
+describe('Test suit', () => {
+  // It block - Test Case
+  it('Test the add method', () => {
+    expect(myObj.add(1, 2)).to.be.equal(3)
+  })
+})
--- src/myClass.js
+++ src/myClass.js
@@ -1,13 +1,14 @@
+// 1 - Created a class
 class MyClass {
   constructor() {
     console.log('Initiate')
   }
-
+  // - Added a method that sums 2 values
   add(arg1, arg2) {
     let result
     result = arg1 + arg2
     return result
   }
 }
-
+// 3 - Export Class
 module.exports = MyClass
